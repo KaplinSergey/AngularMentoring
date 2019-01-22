b2c9a6cd3dcebfa0003e97cb8f98f36e53554f5c
Siarhei Kaplin <Siarhei_Kaplin@epam.com>
2019-01-22 17:26:49 +0300

    ProductComponent refactored

--- src/app/product-component/category.ts
+++
@@ -1,6 +0,0 @@
-enum Category {
-    Beverages,
-    Confections,
-    Meat,
-    Seafood
-}
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from './category';
+
+@Component({
+  selector: 'app-product', // <app-product></app-product>
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  name: string;
+  description: string;
+  price: number;
+  category: Category;
+  isAvailable: boolean;
+  suppliers: string[];
+  ratings: number[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.name = 'Angular for dummies';
+    this.description = 'Angular Tutorial: Learn Angular from scratch step by step';
+    this.price = 100;
+    this.category = Category.Books;
+    this.isAvailable = true;
+    this.suppliers = ['Amazon', 'Ebay'];
+    this.ratings = [10, 9, 8];
+  }
+
+}
