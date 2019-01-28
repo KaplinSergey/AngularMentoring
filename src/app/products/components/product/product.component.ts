4d25a1696a471ce61ca62394780af83640b37335
SergeyKaplin <kaplinsergey85@gmail.com>
2019-01-29 00:54:55 +0300

    Refactored "ProductListComponent'' and "CartComponent"

--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Category } from '../../models/category';
 import { ProductModel } from '../../models/product-model';
-import { CartService } from '../../../cart/services/cart.service';
 
 @Component({
   selector: 'app-product', // <app-product></app-product>
@@ -12,13 +10,8 @@ export class ProductComponent implements OnInit {
   @Input()
   product: ProductModel;
 
-  constructor(private cartService: CartService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onBuy() {
-    console.log(`You have bought this ${this.product.name}`);
-    this.cartService.buyProduct(this.product);
   }
 }
