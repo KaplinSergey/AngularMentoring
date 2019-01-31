63787d0f336730ef0d20f33ebf710799e85c9328
Siarhei Kaplin <Siarhei_Kaplin@epam.com>
2019-01-31 18:19:11 +0300

    Added "Highlight" directive

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { CartComponent } from './components/cart/cart.component';
 import { СartItemComponent } from './components/сart-item/сart-item.component';
-import {FormsModule} from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CartComponent, СartItemComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   exports: [CartComponent]
 })
