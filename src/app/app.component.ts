cda97e2880fce4149d736a3503758e956da594d4
Siarhei Kaplin <Siarhei_Kaplin@epam.com>
2019-01-31 17:29:22 +0300

    Added 'Title' using  "@ViewChild" directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit, OnChanges, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnChanges, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { SimpleChanges } from '@angular/core';
 import { CartService } from './cart/services/cart.service';
 import { Subscription } from 'rxjs';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'shop';
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   showCart: boolean;
   private sub: Subscription;
+
+  @ViewChild('appTitle')
+  title: ElementRef;
 
   constructor(private cartService: CartService) {
   }
@@ -24,5 +27,9 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  ngAfterViewInit() {
+    (<HTMLElement>this.title.nativeElement).innerText = 'Shop';
   }
 }
