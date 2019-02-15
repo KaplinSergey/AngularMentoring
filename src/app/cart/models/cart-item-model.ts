d62ef0ac4679ac13e2ed8fab948939bb1b12ae84
Siarhei Kaplin <Siarhei_Kaplin@epam.com>
2019-02-15 13:33:40 +0300

    Added 'OrderByPipe'

--- src/app/cart/models/cart-item-model.ts
+++ src/app/cart/models/cart-item-model.ts
@@ -3,6 +3,8 @@ import { ProductModel } from '../../products/models/product-model';
 export class CartItemModel {
     constructor(
         public product: ProductModel,
-        public quantity: number
+        public quantity: number,
+        public name = product.name,
+        public price = product.price
     ) {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HighlightDirective } from './directives/highlight.directive';
 import { EnamPipe } from './pipes/enam.pipe';
+import { OrderByPipe } from './pipes/order-by.pipe';
 
 @NgModule({
-  declarations: [HighlightDirective, EnamPipe],
+  declarations: [HighlightDirective, EnamPipe, OrderByPipe],
   imports: [
     CommonModule
   ],
   exports: [
     HighlightDirective,
-    EnamPipe
+    EnamPipe,
+    OrderByPipe
   ]
 })
 export class SharedModule { }
